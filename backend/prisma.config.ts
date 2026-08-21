4e3efd0d98db23b4dd827a8d913a091faba7f234
ajwast <ajwast@gmail.com>
2026-08-21 09:47:44 +0200

    non-null assertion on database url

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
-import type { PrismaConfig } from "prisma";
-// import { env } from "prisma/config";
-export default {
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies PrismaConfig;
+});
